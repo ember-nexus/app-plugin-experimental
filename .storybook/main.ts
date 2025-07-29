c548731d3cea4f6f744d519fea146a4eb72adc1c
Syndesi <soerenklein98@protonmail.com>
2025-07-29 10:46:16 +0200

    wip test ci

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,5 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
 
-// import projectWebpackConfig from '../webpack.config.cjs';
-
 const config: StorybookConfig = {
   stories: ['../storybook/**/*.mdx', '../storybook/**/*.stories.ts'],
   staticDirs: [{ from: '../dist/', to: '/dist' }],
@@ -13,41 +11,6 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
-  },
-  // webpackFinal: async (config) =>
-  // {
-  //   let rules = [...config.module.rules, ...projectWebpackConfig.module.rules];
-  //   let filteredRules = [];
-  //   for (let i = 0; i < rules.length; i++) {
-  //     let rule = rules[i];
-  //     if (!('test' in rule)){
-  //       continue;
-  //     }
-  //     if (rule.test.toString().includes('css')) {
-  //       continue;
-  //     }
-  //     filteredRules.push(rule);
-  //   }
-  //
-  //   filteredRules.push({
-  //     test: /\.css$/,
-  //     oneOf: [
-  //       {
-  //         resourceQuery: /style/,
-  //         use: [ 'style-loader', 'css-loader' ],
-  //       },
-  //       {
-  //         type: 'asset/source',
-  //       },
-  //     ]
-  //   });
-  //
-  //
-  //
-  //   return {
-  //     ...config,
-  //     module: { ...config.module, rules: filteredRules }
-  //   }
-  // },
+  }
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default {
+  base: './',
   build: {
     outDir: 'dist/browser',
     sourcemap: true,
